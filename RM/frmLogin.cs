aab01a60633d9190d61e3c6afb80865964cc43df
hggnomm <hggnomm.dev@gmail.com>
2024-11-23 15:54:30 +0700

    Application.exit()

--- RM/frmLogin.cs
+++ RM/frmLogin.cs
@@ -16,5 +16,10 @@ namespace RM
         {
             InitializeComponent();
         }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
